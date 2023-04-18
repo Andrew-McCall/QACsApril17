c6f45fa60766822233cd631c1b8875bcd96d5261
Andrew-McCall <amccall@qa.com>
2023-04-18 12:40:59 +0100

    vehicle example

--- QACsApril17/Runner.cs
+++ QACsApril17/Runner.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using MathsLibrary;
@@ -11,8 +13,94 @@ namespace QACsApril17
     {
         public static void Main(String[] args)
         {
+            // Arrays (TS)
+            // ArrayLists
+            // List (TS)
 
-            int[] array = new int[0];
+            int[][] array2DIrregular = new int[8][]; 
+            int[,]  array2DRegular = new int[8,5]; 
+
+            IList<List<int>> list2D = new List<List<int>>();
+
+            ArrayList al = new ArrayList();
+            al.Add(new List<int>());
+            al.Add(532);
+            al.Add("string");
+            al.Add(true);
+
+
+            // Not type safe - No type needed
+            // ArrayList array = new ArrayList();
+
+
+            List<int> forLoopDemo = new List<int>();
+            forLoopDemo.Add(0);
+            forLoopDemo.Add(1); 
+            forLoopDemo.Add(2);
+            forLoopDemo.Add(3);
+            forLoopDemo.Add(4);
+
+            Console.WriteLine("forEach");
+
+            foreach (int x in forLoopDemo)
+            {
+                // x is readonly
+                Console.WriteLine(x);
+                // x += 53253;
+            }
+
+            //  starting      condition          operation
+            for (int x = 0; x < forLoopDemo.Count; x++)
+            {
+                Console.WriteLine(forLoopDemo[x]);
+            }
+
+            string[] muppetArray = { "Kermit", "Miss Piggy", "Animal", "Beaker", "Dr Zoot", "Muzzy", "Big Bird" };
+            List<string> muppets1 = muppetArray.ToList<string>();
+            List<string> muppets2 = new List<string>(muppetArray);
+            List<string> muppets3 = new List<string>() { "Kermit", "Miss Piggy", "Animal", "Beaker", "Dr Zoot", "Muzzy", "Big Bird" };
+
+            Console.WriteLine(muppets1[muppets1.Count - 1]);
+            Console.WriteLine(muppets1[^1]);
+
+
+            Console.WriteLine(muppets1[muppets1.Count - 2]);
+            Console.WriteLine(muppets1[^2]);
+
+            Dictionary<string, string> catchPhrases = new Dictionary<string, string>();
+            catchPhrases.Add("Kermit", "Hi ho!");
+            catchPhrases.Add("Piggy", "Hieeeeea!");
+            catchPhrases.Add("Beaker", "Meep Meep!");
+            Console.WriteLine(catchPhrases["Kermit"]);
+            catchPhrases["Kermit"] = "changed!";
+            foreach (string key in catchPhrases.Keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            foreach (string value in catchPhrases.Values)
+            {
+                Console.WriteLine(value);
+            }
+
+            foreach (KeyValuePair<string,string> kvp in catchPhrases)
+            {
+                Console.WriteLine(kvp);
+                Console.WriteLine(kvp.Value);
+                Console.WriteLine(kvp.Key);
+            }
+
+            for (int i = 0; i < muppets1.Count; i++)
+            {
+                Console.WriteLine(muppets1[i].ToCharArray()[0..3]);
+            }
+
+            foreach (string s in muppets1.OrderByDescending(x => x))
+            {
+                Console.WriteLine(s);
+            }
+
+            List<int> array = new List<int>();
             while (true)
             {
                 Console.WriteLine("Enter a number or q to quit:");
@@ -26,22 +114,17 @@ namespace QACsApril17
 
                 int inputParsed = int.Parse(input);
                 
-                int[] shoeHornedArray = new int[array.Length + 1];
-                int i = 0;
-                while (i < array.Length) shoeHornedArray[i] = array[i++];
-                shoeHornedArray[shoeHornedArray.Length - 1] = inputParsed;
-                
-                array = shoeHornedArray;
+                array.Add(inputParsed);
             }
 
             decimal sum = 0;
             int index = 0;
-            while (index < array.Length)
+            while (index < array.Count)
             {
                 sum += array[index++];
             }
 
-            Console.WriteLine(sum / array.Length);
+            Console.WriteLine(sum / array.Count);
 
         }
 
---
+++ QACsApril17/Vehicle.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QACsApril17
+{
+    internal abstract class Vehicle: IDriveable // interface on the base class
+    {
+        public double weight;
+        public double xCoord;
+        public double yCoord;
+        public string owner;
+
+        public Vehicle(string owner)
+        {
+            this.owner = owner;
+        }
+
+        public abstract void Drive(double distance);
+
+        public string getOwner()
+        {
+            return this.owner;
+        }
+    }
+    
+    internal class Car: Vehicle // Inheritance
+    {
+        public int doors;
+
+        public Car(string owner) : base(owner)
+        {}
+
+        public override void Drive(double distance)
+        {
+            this.xCoord += distance;
+            this.yCoord += 0;
+        }
+
+    }
+
+    internal class SportsCar : Car // Inheritance
+    {
+        private Object Valet;
+
+        public SportsCar(string owner) : base(owner)
+        { }
+
+    }
+
+    internal class AbondedCar : Car 
+    {
+        public AbondedCar() : base("No One")
+        { }
+
+    }
+
+    internal class Bike : Vehicle // Inheritance
+    {
+
+        public Bike(string owner) : base(owner)
+        { }
+
+        public override void Drive(double distance)
+        {
+            this.xCoord += distance;
+            this.yCoord += 0;
+        }
+
+    }
+
+
+    internal interface IDriveable
+    {
+        public void Drive(double distance);
+    }
+
+    internal class Driver
+    {
+        // assocation
+        public Vehicle vehicle;
+
+        public void Drive(double distance)
+        {
+            vehicle.Drive(distance);
+        }
+    }
+
+}
