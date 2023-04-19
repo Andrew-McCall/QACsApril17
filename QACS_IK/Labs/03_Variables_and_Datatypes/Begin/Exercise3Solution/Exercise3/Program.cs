257450bb82cfa5aa7c4da2990bed148b3d2a871f
Andrew-McCall <amccall@qa.com>
2023-04-19 14:50:47 +0100

    All Course files

---
+++ QACS_IK/Labs/03_Variables_and_Datatypes/Begin/Exercise3Solution/Exercise3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solution(24);
+        }
+
+    }
+    class Solution
+    {
+        public void solution(int N)
+        {
+            for (int i = 1; i <= N; i++)
+            {
+
+                String output = "";
+
+                if (i % 2 == 0)
+                {
+                    output += "Codility";
+                }
+
+                if (i % 3 == 0)
+                {
+                    output += "Test";
+                }
+
+                if (i % 5 == 0)
+                {
+                    output += "Coders";
+                }
+
+                if (output.Length == 0)
+                {
+                    output = N.ToString();
+                }
+
+                Console.WriteLine(output);
+
+            }
+        }
+    }
+
+}
---
+++ QACS_IK/Labs/13_Exception_Handling/End/CarLibrary/CarLibrary/CarLibrary/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace CarLibrary
+{
+    public class Car
+    {
+
+        public int Speed { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Colour { get; set; }
+
+        public Car() { }
+
+    }
+}
